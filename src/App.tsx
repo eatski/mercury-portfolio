dab15ac0eea93167fbeb058c3c79f393246330cf
hagaitski <i.haga629@gmail.com>
2022-11-06 01:16:56 +0900

    avoid roop

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useId, useMemo, useRef, useState, useSyncExternalStore } from 'react'
 import {InMemoryCache} from "@apollo/client"
 import './App.css'
 import { server } from './server'
 import {gql} from "@apollo/client"
+import React from 'react'
 
 const cache = new InMemoryCache();
 
-const fetchQuery = (query: string)  => {
-  const queryNode = gql(query);
+const tryParseQuery = (query: string) => {
+  try {
+    return gql(query);
+  } catch (e) {
+    return null;
+  }
+}
+
+const useQuery = (query: string)  => {
+  //HACk
+  const ref = useRef(new Set<string>());
+  const queryNode = tryParseQuery(query);
+  if(!queryNode) {
+    return null
+  }
   const result = cache.readQuery({
     query: queryNode
-  })
+  });
   console.log("readQuery",result)
   if(!result){
-    throw server.executeOperation({
-      query: queryNode
-    }).then(res => {
-      console.log("result",res.data);
-      cache.writeQuery({
-        query: queryNode,
-        data: res.data
+    if(ref.current.has(query)){
+      return null
+    } else {
+      ref.current.add(query)
+      throw server.executeOperation({
+        query: queryNode
+      }).then(res => {
+        console.log("result",res.data);
+        if(res.data){
+          res.data && cache.writeQuery({
+            query: queryNode,
+            data: res.data
+          })
+        }
       })
-      return res.data;
-    })
+    }
+   
   }
   return result;
 }
@@ -37,9 +58,7 @@ function App() {
       <div className="card">
         <textarea value={query} onInput={(e) => setQuery((e.target as HTMLTextAreaElement).value)}>
         </textarea>
-        <Suspense fallback={null}>
-          <Result query={query}/>
-        </Suspense>
+        <MemorizedResult query={query}/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -48,9 +67,17 @@ function App() {
   )
 }
 
+const MemorizedResult: React.FC<{query: string}> = ({query}) => {
+  return <Suspense fallback={null}>
+    <Result query={query}/>
+  </Suspense>
+}
+
 const Result: React.FC<{query: string}> = ({query}) => {
+  
+
   return  <div>
-      {JSON.stringify(fetchQuery(query))}
+      {JSON.stringify(useQuery(query))}
     </div>
 }
 
